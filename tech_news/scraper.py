0f5a299baa8dd881b2669d6e0e196b176a42719d
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-17 15:43:13 -0300

    feat: criada a função fetch. função será responsável por fazer a requisição HTTP ao site e obter o conteúdo HTML.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui2"""
+    time.sleep(1)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
