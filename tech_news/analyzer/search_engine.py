2bffa6279c8bf81488f396dba66469a7d7750bf6
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-18 11:53:27 -0300

    feat: search_by_title, funcao que faz buscas por um título passado como parâmetro

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # query = {"title": title}
+    query = {"title": {"$regex": title, "$options": "i"}}
+    result = search_news(query)
+    list_obj = [(news["title"], news["url"]) for news in result]
+    return list_obj
 
 
 # Requisito 7
@@ -16,3 +23,11 @@ def search_by_tag(tag):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# print(search_by_title("bacana"))
+
+# Fontes:
+
+#     Req6: https://stackoverflow.com/questions/10610131/
+#       checking-if-a-field-contains-a-string
