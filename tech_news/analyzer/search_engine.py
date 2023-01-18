019e04717167bb654131ddb24f31f12810e8cc6b
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-18 14:49:21 -0300

    feat: função search_by_date.  função deve receber como parâmetro uma data no formato ISO e # Boas-vindas ao repositório do Tech News

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,17 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+
+def str_to_date(str_iso_date):
+    return datetime.strptime(str_iso_date, "%Y-%m-%d")
+
+
+def format_date(obj_date):
+    day = obj_date.strftime("%d")
+    month = obj_date.strftime("%m")
+    year = obj_date.strftime("%Y")
+
+    return f"{day}/{month}/{year}"
 
 
 # Requisito 6
@@ -12,7 +25,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        obj_date = str_to_date(date)
+        formated_date = format_date(obj_date)
+
+        query = {"timestamp": {"$regex": formated_date}}
+        result = search_news(query)
+        list_obj = [(news["title"], news["url"]) for news in result]
+        return list_obj
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
@@ -25,7 +48,7 @@ def search_by_category(category):
     """Seu código deve vir aqui"""
 
 
-# print(search_by_title("bacana"))
+print(search_by_date("2022-04-07"))
 
 # Fontes:
 
